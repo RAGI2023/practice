fcf3e417842f5e5a141e5f75ba423e29c2afe5b0
yinzhe <yinzhe23@bupt.edu.cn>
2023-09-07 21:06:17 +0800

    [9.7 21]

---
+++ lesser.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int imin(int a, int b);
+int main(void)
+{
+    int a, b;
+    printf("Please enter a pair of integers (q to quit):\n");
+    while ((scanf("%d %d", &a, &b)) == 2)
+    {
+        printf("The lesser of %d and %d is %d.\n", a, b, imin(a, b));
+        printf("Enter a pair of integers (q to quit):\n");
+    }
+
+    return 0;
+}
+
+int imin(int a, int b)
+{
+    int min;
+    min = (a <= b) ? a : b;
+    return min;
+}
